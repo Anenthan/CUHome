c690851b2c96a1546ed86396a53225cbcfa950e8
Anenthan Palakumar <palakuma@coventry.ac.uk>
2019-08-16 00:01:28 +0100

    Add project files.

---
+++ CUHome/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CUHome.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
